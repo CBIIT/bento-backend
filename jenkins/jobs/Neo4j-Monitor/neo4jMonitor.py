387ba6cc33b0f845c9acb061b9dcdc5e8854a4e2
Michael Fleming <michael.fleming@nih.gov>
2020-03-11 10:11:16 -0400

    Neo4j monitoring script
    Initial commit for python script used to monitor the Neo4j API

---
+++ jenkins/jobs/Neo4j-Monitor/neo4jMonitor.py
@@ -0,0 +1,33 @@
+import requests
+from requests.exceptions import ConnectionError
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--servers', nargs='*')
+parser.add_argument('--slackURL')
+
+slackHeaders = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+args = parser.parse_args()
+slack_url = args.slackURL
+
+# POST function to send to Slack
+def post_message_to_slack(message_text):
+    return requests.post(slack_url, data=message_text, headers=slackHeaders)
+
+#example args ['ncias-d2224-c','ncias-d2267-c']:
+for i in args.servers:
+    graphURL = 'http://' + i + '.nci.nih.gov:7475/graphql/'
+    print graphURL
+
+    try:
+        request = requests.get(graphURL)
+    except ConnectionError:
+        print('The graph QL endpoint on ' + i + ' is NOT responding - please verify that this application is working')
+        slack_message = 'The graph QL endpoint on {} is NOT responding - please verify that this application is working'.format(i)
+        payload = '{"text":"' + slack_message + '"}'
+        print payload
+        post_message_to_slack(payload)
+
+    else:
+        print('The graph QL endpoint on ' + i + ' is available')
+~

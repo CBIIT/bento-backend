b25eaea29808509aa3148d06396d1ac13808d12c
Michael Fleming <michael.fleming@nih.gov>
2020-03-12 12:59:24 -0400

    Neo4j Monitor Script
    added graphql query to the api call to neo4j

--- jenkins/jobs/Neo4j-Monitor/neo4jMonitor.sh
+++ jenkins/jobs/Neo4j-Monitor/neo4jMonitor.sh
@@ -2,22 +2,27 @@
 
 serverList=$1
 slack_url=$2
+dbCreds=$3
 slackHeaders="\"Content-type: application/json\""
+queryString="'{\"query\":\"{numberOfCases}\"}'"
 
 # POST function to send to Slack
 post_message_to_slack(){
-        curlCMD="curl --request POST --header $slackHeaders --data $payload $slack_url"
+        curlCMD="curl --silent --max-time 10 --request POST --header $slackHeaders --data $payload $slack_url"
         slackError=$(eval $curlCMD)
         #echo "Slack Error:   $slackError"
         }
 
 for i in $serverList
 do
-    graphURL="http://$i.nci.nih.gov:7474/graphql/"
-    error=$(curl --write-out %{http_code} --silent --output /dev/null $graphURL)
+    graphURL="http://$dbCreds@$i.nci.nih.gov:7474/graphql/"
+    #error=$(curl --write-out %{http_code} --silent --output /dev/null $graphURL)
+    graphReq="curl --silent --max-time 10 --request POST --header $slackHeaders --data $queryString $graphURL"
+    error=$(eval $graphReq)
     #echo $error
 
-    if [[ $error -eq '200' ]] || [[ $error -eq '401' ]]
+    #if [[ $error -eq '200' ]] || [[ $error -eq '401' ]]
+    if [[ $error == *"\"data\":{\"numberOfCases\":"* ]]
     then
         echo "The graph QL endpoint on $i is available"
     else

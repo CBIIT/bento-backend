31f1ac1ce7c49bac5326984b8307a9f91a6ba610
Vincent Donkor <vincent.donkor@gmail.com>
2020-04-13 15:24:22 -0400

    added bento

---
+++ bento/frontend/dist/precache-manifest.bd1bfb27f8e2f98e0b8e88edc0989364.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = [
+  {
+    "revision": "57de0f14db17b13dc2a8",
+    "url": "./static/js/main.57de0f14.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "d0fd889910a603b0b064",
+    "url": "./static/js/2.d0fd8899.chunk.js"
+  },
+  {
+    "revision": "79bff0a1361235a2e46e8d930ba82e64",
+    "url": "./static/media/About-ExternalLink.79bff0a1.svg"
+  },
+  {
+    "revision": "b6d59d61844c1b6517fcc210db0fc6f0",
+    "url": "./static/media/Icon-MyCases.b6d59d61.svg"
+  },
+  {
+    "revision": "7c12621d5fca8a09d5a29cf2dadc54f3",
+    "url": "./static/media/LP_ReadMore.7c12621d.svg"
+  },
+  {
+    "revision": "3c5f782b280525ea696ba05a39d3d60b",
+    "url": "./static/media/Icon-StudiesDetail.3c5f782b.svg"
+  },
+  {
+    "revision": "827a218ff3318c3d6ea6794d99453cb2",
+    "url": "./static/media/Icon-Programs.827a218f.svg"
+  },
+  {
+    "revision": "2788dd101c3f3f503e049f9c8fbe0b81",
+    "url": "./static/media/Photo-About_ICDC_model.2788dd10.jpg"
+  },
+  {
+    "revision": "2376f213bbc1cf3863d883da4ef74e3d",
+    "url": "./static/media/ICDC_DGAB_Guidelines.2376f213.pdf"
+  },
+  {
+    "revision": "cbb608d11f320ec7617fb7878d0a45c1",
+    "url": "./static/media/icdc_nih_logo.cbb608d1.svg"
+  },
+  {
+    "revision": "ccbca4f90d956acb127d3cd754bc1bf8",
+    "url": "./static/media/Canine400.ccbca4f9.png"
+  },
+  {
+    "revision": "65953a9b1ec15d0ebb1b451f28caa213",
+    "url": "./static/media/Canine800.65953a9b.png"
+  },
+  {
+    "revision": "f3f4b62e503c7fe5c01dccecc7ed32ee",
+    "url": "./static/media/Canine1400.f3f4b62e.png"
+  },
+  {
+    "revision": "1a181226de12abd856fa7a38cbda5312",
+    "url": "./static/media/Canine1600.1a181226.png"
+  },
+  {
+    "revision": "857a7519efa3d53552197c2e517f3a91",
+    "url": "./static/media/Canine1800.857a7519.png"
+  },
+  {
+    "revision": "485bbdc1023cb4d3005b7e6b5bba8eeb",
+    "url": "./static/media/Canine2000.485bbdc1.png"
+  },
+  {
+    "revision": "bfb7afcb513d954f20c757ae96f1c011",
+    "url": "./static/media/Icon-funnel-blue.bfb7afcb.svg"
+  },
+  {
+    "revision": "505d02368bdc6eac6b8fddaa4d070534",
+    "url": "./static/media/Icon-funnel-white.505d0236.svg"
+  },
+  {
+    "revision": "7760dca31dd88c295678ee4a22ef4e03",
+    "url": "./static/media/NCI-footer.logo.7760dca3.svg"
+  },
+  {
+    "revision": "fc589d9b3f24e3b4e96c88f19c3f7cee",
+    "url": "./static/media/canine_helix.fc589d9b.jpg"
+  },
+  {
+    "revision": "b2687974bc0ffc0bd56f5cbe824c7e2b",
+    "url": "./static/media/Icon-studies-stats.b2687974.svg"
+  },
+  {
+    "revision": "eae785d223ff2538317bde92b2de64e0",
+    "url": "./static/media/Icon-cases-stats.eae785d2.svg"
+  },
+  {
+    "revision": "0507390f1ff9d126327b99807bf73ed1",
+    "url": "./static/media/Icon-samples-stats.0507390f.svg"
+  },
+  {
+    "revision": "4d6d57e8135b99937b967ff7d306f758",
+    "url": "./static/media/Icon-files-stats.4d6d57e8.svg"
+  },
+  {
+    "revision": "ac9f3a0f0e86a1262e45711613f36b4e",
+    "url": "./static/media/Icon-aliquots-stats.ac9f3a0f.svg"
+  },
+  {
+    "revision": "342f00410f8a50e2d0fc1f63f75d1d90",
+    "url": "./static/media/Icon-CaseDetail.342f0041.svg"
+  },
+  {
+    "revision": "538901635615359f40ef977988250b9e",
+    "url": "./static/media/Program-ExternalLink.53890163.svg"
+  },
+  {
+    "revision": "3c35fe81d4a2cf44de896d48579a5aa7",
+    "url": "./static/media/Program_Image.3c35fe81.png"
+  },
+  {
+    "revision": "f88c1c7b0c1784e5d966e1bcc73340c7",
+    "url": "./static/media/Model-Buttons-Center.f88c1c7b.svg"
+  },
+  {
+    "revision": "84bb747ace7bdb8af694aa7af778a48c",
+    "url": "./static/media/Model-Buttons-ZoomIn.84bb747a.svg"
+  },
+  {
+    "revision": "cafff3432edf8a367b914110d2b19397",
+    "url": "./static/media/Model-Buttons-ZoomOut.cafff343.svg"
+  },
+  {
+    "revision": "da056abe277a7c9241ccefa67ff697e9",
+    "url": "./static/media/ProgramDetail_Image.da056abe.jpg"
+  },
+  {
+    "revision": "eb1576b88fbc33f3945f377b98f4fc57",
+    "url": "./static/media/LP_About.eb1576b8.png"
+  },
+  {
+    "revision": "5d6fbfda4940483f86654efb89821704",
+    "url": "./static/media/LP_Program.5d6fbfda.png"
+  },
+  {
+    "revision": "04e250e041488882ba93d499b00ed542",
+    "url": "./static/media/LP_Studies.04e250e0.png"
+  },
+  {
+    "revision": "7b480c91012b808cf65b8cddcdb43008",
+    "url": "./static/media/LP_Submit.7b480c91.png"
+  },
+  {
+    "revision": "f3c570be42a0359b433177d84cb06bc5",
+    "url": "./static/media/LP_About_Fullarticle.Arrow.f3c570be.svg"
+  },
+  {
+    "revision": "8bc303038113f8d539dc5ecc95dee41a",
+    "url": "./static/media/LP-Background.1400x1600.8bc30303.jpg"
+  },
+  {
+    "revision": "ce39a727c611ae9314e7b016d24eb330",
+    "url": "./static/media/LP_Cases.ce39a727.png"
+  },
+  {
+    "revision": "0a6d963c2da3873b759dd5077cc6bb4b",
+    "url": "./static/media/LP_FLARE.2.0a6d963c.png"
+  },
+  {
+    "revision": "9c5177b0e322ef1e3a1e1e27440d01f6",
+    "url": "./static/media/dog-bubble.9c5177b0.png"
+  },
+  {
+    "revision": "6407d9ec9c3add12120269ed92fe38bc",
+    "url": "./static/media/human-bubble.6407d9ec.png"
+  },
+  {
+    "revision": "6293dd0309ec69500aa78a36f6b811ab",
+    "url": "./static/media/Photo-About_CRDC.6293dd03.jpg"
+  },
+  {
+    "revision": "2b16ea014d44d07b557000ef9707034e",
+    "url": "./static/media/Photo-About_Developer.2b16ea01.jpg"
+  },
+  {
+    "revision": "de859eddb79a32b71df6cff916800eba",
+    "url": "./static/media/Photo-About_Purpose.de859edd.jpg"
+  },
+  {
+    "revision": "cfdf0ca76dfba8ae6cc67415f7967a7e",
+    "url": "./static/media/Photo-About_SteeringCommittee.cfdf0ca7.jpg"
+  },
+  {
+    "revision": "5a93da24758a3144062d6805355410e5",
+    "url": "./static/media/About_Support.5a93da24.jpg"
+  },
+  {
+    "revision": "3c845d72fcca34f7b7da23695b9e0f5e",
+    "url": "./static/media/Photo-About_SubmittingData.3c845d72.jpg"
+  },
+  {
+    "revision": "b9c233a7b6e7ba96ece3d6b9e4e830a3",
+    "url": "./index.html"
+  }
+];

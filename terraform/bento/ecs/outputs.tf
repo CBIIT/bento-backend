c549c4f046507f6fb3bbb09a14aa6312eb410e1a
Vincent Donkor <vincent.donkor@gmail.com>
2020-05-22 01:16:18 -0400

    updated ecs

--- terraform/bento/ecs/outputs.tf
+++ terraform/bento/ecs/outputs.tf
@@ -7,3 +7,13 @@ output "bastion_ip" {
   description = "ip address of bastion host"
   value       = data.terraform_remote_state.network.outputs.bastion_public_ip
 }
+
+output "alb_arn" {
+  value = aws_alb.alb.arn
+}
+output "alb_dns" {
+  value = aws_alb.alb.dns_name
+}
+output "zone_id" {
+  value = aws_alb.alb.zone_id
+}

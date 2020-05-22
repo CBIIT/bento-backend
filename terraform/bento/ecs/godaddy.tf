93abc5b2a279e18e5e46d7559afd1a7a8af4a82c
Vincent Donkor <vincent.donkor@gmail.com>
2020-05-22 01:16:18 -0400

    updated ecs

--- terraform/bento/ecs/godaddy.tf
+++ terraform/bento/ecs/godaddy.tf
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #data "aws_ssm_parameter" "dns_api_key" {
 #  name = "dns_api_key"
 #}
@@ -9,18 +9,18 @@
 #    key = data.aws_ssm_parameter.dns_api_key.value
 #    secret = data.aws_ssm_parameter.dns_api_secret.value
 #}
-=======
-data "aws_ssm_parameter" "dns_api_key" {
-  name = "dns_api_key"
-}
-data "aws_ssm_parameter" "dns_api_secret" {
-  name = "dns_api_secret"
-}
-provider "godaddy" {
-    key = data.aws_ssm_parameter.dns_api_key.value
-    secret = data.aws_ssm_parameter.dns_api_secret.value
-}
->>>>>>> b9b506ce97e3b5d8317edd028dc7d20d4467d569
+
+//data "aws_ssm_parameter" "dns_api_key" {
+//  name = "dns_api_key"
+//}
+//data "aws_ssm_parameter" "dns_api_secret" {
+//  name = "dns_api_secret"
+//}
+//provider "godaddy" {
+//    key = data.aws_ssm_parameter.dns_api_key.value
+//    secret = data.aws_ssm_parameter.dns_api_secret.value
+//}
+
 
 # resource "godaddy_domain_record" "essential-dev" {
 #   domain    = var.domain

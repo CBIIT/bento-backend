34f409fe76e1b20a02091cddf30f876e554a257b
Austin Mueller <austinsmueller@gmail.com>
2020-07-21 10:37:40 -0400

    Added assert() to GraphQLController Testing

--- src/test/java/gov/nih/nci/bento/GraphQLControllerTests.java
+++ src/test/java/gov/nih/nci/bento/GraphQLControllerTests.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.junit.Assert.assertTrue;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -38,17 +39,24 @@ public class GraphQLControllerTests {
 				.build();
 	}
 
-
 	@Test
-	public void testGraphQLEndPointWithInValidPayLoad() throws Exception {
-		this.mockMvc
-				.perform(RestDocumentationRequestBuilders
-					.post("/v1/graphql/")
-					.contentType(MediaType.APPLICATION_JSON)
-					.content("{\"query\":\"{e{id}}\"}"))
-				.andDo(print())
-				.andExpect(status()
-					.is4xxClientError())
-				.andDo(document("{ClassName}/{methodName}"));
+	public void testGraphQLEndPointWithInValidPayLoad(){
+		boolean successful;
+		try{
+			this.mockMvc
+					.perform(RestDocumentationRequestBuilders
+							.post("/v1/graphql/")
+							.contentType(MediaType.APPLICATION_JSON)
+							.content("{\"query\":\"{e{id}}\"}"))
+					.andDo(print())
+					.andExpect(status()
+							.is4xxClientError())
+					.andDo(document("{ClassName}/{methodName}"));
+			successful = true;
+		}
+		catch (Exception e){
+			successful = false;
+		}
+		assertTrue(successful);
 	}
 }

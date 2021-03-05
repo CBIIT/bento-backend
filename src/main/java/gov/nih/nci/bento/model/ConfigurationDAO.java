5ea76dbc15c5213d0addb2b6a6ef865e2e4c8c84
Austin Mueller <austinsmueller@gmail.com>
2021-03-05 16:30:14 -0500

    Removed unused imports

--- src/main/java/gov/nih/nci/bento/model/ConfigurationDAO.java
+++ src/main/java/gov/nih/nci/bento/model/ConfigurationDAO.java
@@ -1,9 +1,7 @@
 package gov.nih.nci.bento.model;
-import gov.nih.nci.bento.BentoApplication;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;

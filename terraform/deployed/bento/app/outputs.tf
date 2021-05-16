321760b44d6b4cd034dbab4892cb48a1ace0bcd0
cloudprofessionals <mycloudprofessionals@gmail.com>
2021-05-16 16:34:44 -0400

    added in project

--- terraform/deployed/bento/app/outputs.tf
+++ terraform/deployed/bento/app/outputs.tf
@@ -2,3 +2,6 @@
 //output "configuration_endpoint_address" {
 //  value = aws_elasticache_replication_group.replication_group.configuration_endpoint_address
 //}
+output "configuration_endpoint_address" {
+  value = aws_elasticache_cluster.redis_cluster.configuration_endpoint
+}

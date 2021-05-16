a68319abbb028a0fe8961e23d9feb1211ad0c8dd
cloudprofessionals <mycloudprofessionals@gmail.com>
2021-05-15 21:54:33 -0400

    added parameters to downloader

--- terraform/deployed/bento/app/outputs.tf
+++ terraform/deployed/bento/app/outputs.tf
@@ -1,4 +1,4 @@
 
-output "configuration_endpoint_address" {
-  value = aws_elasticache_replication_group.replication_group.configuration_endpoint_address
-}
+//output "configuration_endpoint_address" {
+//  value = aws_elasticache_replication_group.replication_group.configuration_endpoint_address
+//}

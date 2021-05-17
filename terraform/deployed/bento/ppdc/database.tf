adf0c0f20d021e5960cefd4f71318baa1faa96d8
cloudprofessionals <mycloudprofessionals@gmail.com>
2021-05-17 11:01:30 -0400

    modified in project

--- terraform/deployed/bento/ppdc/database.tf
+++ terraform/deployed/bento/ppdc/database.tf
@@ -74,12 +74,12 @@ mainSteps:
     - rm -rf icdc-devops || true
     - yum -y install epel-release
     - yum -y install wget git python-setuptools python-pip
+    - pip install --upgrade "pip < 21.0"
     - pip install ansible==2.8.0 boto boto3 botocore
     - git clone https://github.com/CBIIT/icdc-devops
-    - cd icdc-devops && git checkout master
-    - cd icrp
-    - ansible-playbook community-neo4j.yml
-    - systemctl restart neo4j
+    - cd icdc-devops/ansible && git checkout master
+    - ansible-playbook open-target-database.yml
+
 DOC
   tags = merge(
   {

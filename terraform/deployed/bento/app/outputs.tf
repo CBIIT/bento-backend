f922fc49441019c85f86aa5389937c754e0582aa
cloudprofessionals <mycloudprofessionals@gmail.com>
2021-05-19 10:26:14 -0400

    updated filedownloader

--- terraform/deployed/bento/app/outputs.tf
+++ terraform/deployed/bento/app/outputs.tf
@@ -1,7 +1,7 @@
 
-//output "configuration_endpoint_address" {
-//  value = aws_elasticache_replication_group.replication_group.configuration_endpoint_address
-//}
 output "configuration_endpoint_address" {
-  value = aws_elasticache_cluster.redis_cluster.configuration_endpoint
+  value = aws_elasticache_replication_group.replication_group.configuration_endpoint_address
 }
+//output "configuration_endpoint_address" {
+//  value = aws_elasticache_cluster.redis_cluster.configuration_endpoint
+//}

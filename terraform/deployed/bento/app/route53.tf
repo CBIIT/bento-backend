b9fe6504e765b2d0971fa6b9393a9a5ffca72db7
cloudprofessionals <mycloudprofessionals@gmail.com>
2021-05-19 13:10:34 -0400

    added backend file service

--- terraform/deployed/bento/app/route53.tf
+++ terraform/deployed/bento/app/route53.tf
@@ -14,7 +14,7 @@ resource "aws_route53_record" "www" {
 
 resource "aws_route53_record" "prod_tier_records" {
   count =  var.env ==  "prod" ? 1 : 0
-  name = var.domain_name
+  name = "${lower(var.stack_name)}.${var.domain_name}"
   type = "A"
   zone_id = data.aws_route53_zone.zone.zone_id
   alias {
@@ -25,8 +25,8 @@ resource "aws_route53_record" "prod_tier_records" {
 }
 
 resource "aws_route53_record" "lower_tiers_records" {
-  count =  var.env ==  "prod" ? 0 : 1
-  name = var.env
+  count =  var.env !=  "prod" ? 1 : 0
+  name ="${lower(var.stack_name)}-${var.env}"
   type = "A"
   zone_id = data.aws_route53_zone.zone.zone_id
   alias {
@@ -36,26 +36,26 @@ resource "aws_route53_record" "lower_tiers_records" {
   }
 }
 
-resource "aws_route53_record" "api" {
-  count =  var.env ==  "prod" ? 0:1
-  name = "api-${var.env}"
-  type = "A"
-  zone_id = data.aws_route53_zone.zone.zone_id
-  alias {
-    evaluate_target_health = false
-    name = module.alb.alb_dns_name
-    zone_id = module.alb.alb_zone_id
-  }
-}
+//resource "aws_route53_record" "api" {
+//  count =  var.env ==  "prod" ? 0:1
+//  name = "api-${var.env}"
+//  type = "A"
+//  zone_id = data.aws_route53_zone.zone.zone_id
+//  alias {
+//    evaluate_target_health = false
+//    name = module.alb.alb_dns_name
+//    zone_id = module.alb.alb_zone_id
+//  }
+//}
 
-resource "aws_route53_record" "api_prod" {
-  count =  var.env ==  "prod" ? 1 : 0
-  name = "api"
-  type = "A"
-  zone_id = data.aws_route53_zone.zone.zone_id
-  alias {
-    evaluate_target_health = false
-    name = module.alb.alb_dns_name
-    zone_id = module.alb.alb_zone_id
-  }
-}
+//resource "aws_route53_record" "api_prod" {
+//  count =  var.env ==  "prod" ? 1 : 0
+//  name = "api"
+//  type = "A"
+//  zone_id = data.aws_route53_zone.zone.zone_id
+//  alias {
+//    evaluate_target_health = false
+//    name = module.alb.alb_dns_name
+//    zone_id = module.alb.alb_zone_id
+//  }
+//}

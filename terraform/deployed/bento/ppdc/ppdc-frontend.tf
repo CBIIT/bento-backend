7d409fcdca87ac005da8df698402a5629136d87e
cloudprofessionals <mycloudprofessionals@gmail.com>
2021-05-25 17:01:36 -0400

    removed otp from ppdc stack

--- terraform/deployed/bento/ppdc/ppdc-frontend.tf
+++ terraform/deployed/bento/ppdc/ppdc-frontend.tf
@@ -1,59 +1,59 @@
 
-resource "aws_instance" "ppdc_otp" {
+resource "aws_instance" "ppdc_frontend" {
   ami          =  data.aws_ami.centos.id
   instance_type     =  var.fronted_instance_type
   iam_instance_profile = aws_iam_instance_profile.ecs-instance-profile.name
-  vpc_security_group_ids   = [aws_security_group.ppdc_otp_security_group.id]
+  vpc_security_group_ids   = [aws_security_group.ppdc_frontend_security_group.id]
   subnet_id  = data.terraform_remote_state.network.outputs.private_subnets_ids[0]
   key_name    =  var.ssh_key_name
   user_data   =  data.template_cloudinit_config.user_data.rendered
   tags = merge(
   {
-    Name        = "${var.stack_name}-otp-${var.env}"
+    Name        = "${var.stack_name}-${var.frontend_app_name}-frontend-${var.env}"
   },
   var.tags,
   )
 }
 
-resource "aws_security_group_rule" "ppdc_otp_bastion_host_ssh" {
+resource "aws_security_group_rule" "ppdc_frontend_bastion_host_ssh" {
   from_port = local.bastion_port
   protocol = local.tcp_protocol
   to_port = local.bastion_port
   source_security_group_id = data.terraform_remote_state.bastion.outputs.bastion_security_group_id
-  security_group_id = aws_security_group.ppdc_otp_security_group.id
+  security_group_id = aws_security_group.ppdc_frontend_security_group.id
   type = "ingress"
 }
 
-resource "aws_security_group_rule" "ppdc_otp_inbound_alb_http" {
+resource "aws_security_group_rule" "ppdc_frontend_inbound_alb_http" {
   from_port = local.http_port
   protocol = local.tcp_protocol
   to_port = local.http_port
-  security_group_id = aws_security_group.ppdc_otp_security_group.id
+  security_group_id = aws_security_group.ppdc_frontend_security_group.id
   source_security_group_id = module.alb.alb_security_group_id
   type = "ingress"
 }
 
 
-#create ppdc-otp security group
-resource "aws_security_group" "ppdc_otp_security_group" {
-  name = "${var.stack_name}-otp-${var.env}-sg"
-  description = "ppdc-otp security group"
+#create ppdc-frontend security group
+resource "aws_security_group" "ppdc_frontend_security_group" {
+  name = "${var.stack_name}-${var.frontend_app_name}-frontend-${var.env}-sg"
+  description = "ppdc-frontend security group"
   vpc_id = data.terraform_remote_state.network.outputs.vpc_id
   tags = merge(
   {
-    "Name" = format("%s-%s",var.stack_name,"ppdc-otp-sg")
+    "Name" = format("%s-%s",var.stack_name,"ppdc-frontend-sg")
   },
   var.tags,
   )
 }
 
 
-resource "aws_security_group_rule" "ppdc_otp_all_outbound" {
+resource "aws_security_group_rule" "ppdc_frontend_all_outbound" {
   from_port = local.any_port
   protocol = local.any_protocol
   to_port = local.any_port
   cidr_blocks = local.all_ips
-  security_group_id = aws_security_group.ppdc_otp_security_group.id
+  security_group_id = aws_security_group.ppdc_frontend_security_group.id
   type = "egress"
 }
 
@@ -61,8 +61,8 @@ resource "aws_security_group_rule" "ppdc_otp_all_outbound" {
 
 
 #create alb target group
-resource "aws_lb_target_group" "ppdc_otp_target_group" {
-  name = "${var.stack_name}-otp-${var.env}-target-group"
+resource "aws_lb_target_group" "ppdc_frontend_target_group" {
+  name = "${var.stack_name}-${var.frontend_app_name}-frontend-${var.env}-target-group"
   port = local.http_port
   protocol = "HTTP"
   vpc_id = data.terraform_remote_state.network.outputs.vpc_id
@@ -82,22 +82,22 @@ resource "aws_lb_target_group" "ppdc_otp_target_group" {
   }
   tags = merge(
   {
-    "Name" = format("%s-%s-%s",var.stack_name,var.env,"otp-alb-target")
+    "Name" = format("%s-%s-%s",var.stack_name,var.env,"frontend-alb-target")
   },
   var.tags,
   )
 }
 
-resource "aws_lb_listener_rule" "ppdc_otp_alb_listener" {
+resource "aws_lb_listener_rule" "ppdc_frontend_alb_listener" {
   listener_arn = module.alb.alb_https_listener_arn
   action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.ppdc_otp_target_group.arn
+    target_group_arn = aws_lb_target_group.ppdc_frontend_target_group.arn
   }
 
   condition {
     host_header {
-      values = ["${var.stack_name}-otp-${var.env}.${var.domain_name}"]
+      values = ["${var.stack_name}-${var.frontend_app_name}$-{var.env}.${var.domain_name}"]
     }
   }
   condition {
@@ -108,15 +108,15 @@ resource "aws_lb_listener_rule" "ppdc_otp_alb_listener" {
 
 }
 
-resource "aws_lb_target_group_attachment" "ppdc_otp" {
-  target_group_arn = aws_lb_target_group.ppdc_otp_target_group.arn
-  target_id = aws_instance.ppdc_otp.id
+resource "aws_lb_target_group_attachment" "ppdc_frontend" {
+  target_group_arn = aws_lb_target_group.ppdc_frontend_target_group.arn
+  target_id = aws_instance.ppdc_frontend.id
   port      = local.http_port
 }
 
 #create boostrap script to hook up the node to ecs cluster
-resource "aws_ssm_document" "ppdc_otp_boostrap" {
-  name          = "${var.stack_name}-initialize-otp-${var.env}"
+resource "aws_ssm_document" "ppdc_frontend_boostrap" {
+  name          = "${var.stack_name}-${var.frontend_app_name}-initialize-frontend-${var.env}"
   document_type = "Command"
   document_format = "YAML"
   content = <<DOC
@@ -148,20 +148,20 @@ DOC
   )
 }
 
-resource "aws_ssm_association" "ppdc_otp_ssm" {
-  name = aws_ssm_document.ppdc_otp_boostrap.name
+resource "aws_ssm_association" "ppdc_frontend_ssm" {
+  name = aws_ssm_document.ppdc_frontend_boostrap.name
   targets {
     key   = "tag:Name"
-    values = ["${var.stack_name}-otp-${var.env}"]
+    values = ["${var.stack_name}-${var.frontend_app_name}-frontend-${var.env}"]
   }
 }
 
-data "aws_route53_zone" "ppdc_otp_zone" {
+data "aws_route53_zone" "ppdc_frontend_zone" {
   name  = var.domain_name
 }
 
-resource "aws_route53_record" "ppdc_otp_records" {
-  name = "${var.stack_name}-otp-${var.env}"
+resource "aws_route53_record" "ppdc_frontend_records" {
+  name = "${var.stack_name}-${var.frontend_app_name}-${var.env}"
   type = "A"
   zone_id = data.aws_route53_zone.zone.zone_id
   alias {

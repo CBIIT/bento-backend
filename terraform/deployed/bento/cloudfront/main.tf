1c640821bf272f0f374591faa47a313f15dfb010
cloudprofessionals <mycloudprofessionals@gmail.com>
2021-06-01 19:32:09 -0400

    updated cloudfront

--- terraform/deployed/bento/cloudfront/main.tf
+++ terraform/deployed/bento/cloudfront/main.tf
@@ -92,7 +92,7 @@ resource "aws_wafv2_web_acl" "waf" {
   scope       = "CLOUDFRONT"
 
   default_action {
-    block {}
+    allow {}
   }
 
   rule {
@@ -100,7 +100,7 @@ resource "aws_wafv2_web_acl" "waf" {
     priority = 1
 
     action {
-      count {}
+      block {}
     }
 
     statement {

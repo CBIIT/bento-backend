f8929879aa44ba51928eb5b9b963621a109a3263
Michael Fleming <michael.fleming@nih.gov>
2021-06-28 16:31:28 -0400

    updated tagging and resource naming

--- cdk/awscdk/bento/aws/albListener.py
+++ cdk/awscdk/bento/aws/albListener.py
@@ -1,33 +1,48 @@
 from aws_cdk import core
 from aws_cdk import aws_elasticloadbalancingv2 as elbv2
+from aws_cdk import aws_certificatemanager as cfm
 
 class ALBListener:
   def createResources(self, ns):
 
     # Attach ALB to ECS Service
-    health_check = elbv2.HealthCheck(interval=core.Duration.seconds(60),
-        path="/",
+    be_health_check = elbv2.HealthCheck(interval=core.Duration.seconds(60),
+        path="/ping",
         timeout=core.Duration.seconds(5))
 
+    self.bentoALB.add_redirect(
+        source_protocol=elbv2.ApplicationProtocol.HTTP,
+        source_port=80,
+        target_protocol=elbv2.ApplicationProtocol.HTTPS,
+        target_port=443)
+
+    bento_cert = cfm.Certificate.from_certificate_arn(self,
+        "bento-cert",
+        certificate_arn=self.config[ns]['cert_arn'])
+
     listener = self.bentoALB.add_listener("PublicListener",
-        port=80
-        )
+        certificates=[
+            bento_cert
+        ],
+        port=443)
 
     frontendtarget = listener.add_targets("ECS-frontend-Target",
-        port=80,
+        port=self.config[ns]['frontend_container_port'],
         targets=[self.frontendService],
-        health_check=health_check)
+        target_group_name="{}-frontend".format(ns))
+    core.Tags.of(frontendtarget).add("Name", "{}-frontend-alb-target".format(ns))
 
     backendtarget = listener.add_targets("ECS-backend-Target",
-        port=8080,
+        port=self.config[ns]['backend_container_port'],
         targets=[self.backendService],
-        health_check=health_check)
+        health_check=be_health_check,
+        target_group_name="{}-backend".format(ns))
+    core.Tags.of(backendtarget).add("Name", "{}-backend-alb-target".format(ns))
 
     # Add a fixed error message when browsing an invalid URL
     listener.add_action("ECS-Content-Not-Found",
         action=elbv2.ListenerAction.fixed_response(200,
-            message_body="The requested resource is not available")
-        )
+            message_body="The requested resource is not available"))
 
     elbv2.ApplicationListenerRule(self, id="alb_frontend_rule",
         path_pattern="/*",

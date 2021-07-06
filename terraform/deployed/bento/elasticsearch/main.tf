ddf483d124630e8efafcec4fcd6ea1eabc3d9d49
cloudprofessionals <mycloudprofessionals@gmail.com>
2021-07-06 10:10:01 -0400

    added elasticsearch

---
+++ terraform/deployed/bento/elasticsearch/main.tf
@@ -0,0 +1,125 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  profile = var.profile
+  region = var.region
+}
+#set the backend for state file
+terraform {
+  backend "s3" {
+    bucket = "bento-terraform-remote-state"
+    key = "bento/elasticsearch/terraform.tfstate"
+    workspace_key_prefix = "env"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
+
+locals {
+  http_port = 80
+  any_port = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  https_port = "443"
+  all_ips  = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group" "es" {
+  name = "${var.stack_name}-${terraform.workspace}-elasticsearch-sg"
+  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
+
+  ingress {
+    from_port = local.https_port
+    to_port = local.https_port
+    protocol = local.any_protocol
+    cidr_blocks = [
+      data.terraform_remote_state.network.outputs.vpc_cidr_block
+    ]
+  }
+}
+
+resource "aws_iam_service_linked_role" "es" {
+  aws_service_name = "es.amazonaws.com"
+}
+
+resource "aws_elasticsearch_domain" "es" {
+  domain_name = var.domain_name
+  elasticsearch_version = "7.8"
+
+  cluster_config {
+    instance_count = 2
+    instance_type = var.elasticsearch_instance_type
+    zone_awareness_enabled = true
+
+    zone_awareness_config {
+      availability_zone_count = 2
+    }
+  }
+
+  vpc_options {
+    subnet_ids = [
+      data.terraform_remote_state.network.outputs.private_subnets_ids[0],
+      data.terraform_remote_state.network.outputs.private_subnets_ids[1]
+    ]
+
+    security_group_ids = [aws_security_group.es.id]
+  }
+
+  ebs_options {
+    ebs_enabled = true
+    volume_size = 120
+  }
+
+  access_policies = <<CONFIG
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Action": "es:*",
+          "Principal": "*",
+          "Effect": "Allow",
+          "Resource": "arn:aws:es:${data.aws_region.region.name}:${data.aws_caller_identity.caller.account_id}:domain/${var.domain_name}/*"
+      }
+  ]
+}
+  CONFIG
+  snapshot_options {
+    automated_snapshot_start_hour = 23
+  }
+  log_publishing_options {
+    cloudwatch_log_group_arn = aws_cloudwatch_log_group.cloudwatch_log_group.arn
+    log_type                 = "INDEX_SLOW_LOGS"
+  }
+  tags = var.tags
+}
+
+
+resource "aws_cloudwatch_log_group" "cloudwatch_log_group" {
+  name = "${var.stack_name}-${terraform.workspace}-es-log-group"
+}
+
+resource "aws_cloudwatch_log_resource_policy" "cloudwatch_policy" {
+  policy_name = "${var.stack_name}-${terraform.workspace}-es-log-policy"
+
+  policy_document = <<CONFIG
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "es.amazonaws.com"
+      },
+      "Action": [
+        "logs:PutLogEvents",
+        "logs:PutLogEventsBatch",
+        "logs:CreateLogStream"
+      ],
+      "Resource": "arn:aws:logs:*"
+    }
+  ]
+}
+CONFIG
+}

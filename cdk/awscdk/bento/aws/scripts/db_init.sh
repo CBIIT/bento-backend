1331914332b8032198daeee0a06cef20228887b6
Michael Fleming <michael.fleming@nih.gov>
2021-07-16 08:39:53 -0400

    updated scripts for initial neo4j install

--- cdk/awscdk/bento/aws/scripts/db_init.sh
+++ cdk/awscdk/bento/aws/scripts/db_init.sh
@@ -5,7 +5,7 @@ rm -rf icdc-devops || true
 yum -y install epel-release
 yum -y install wget git python-setuptools python-pip
 pip install --upgrade "pip < 21.0"
-pip install ansible==2.8.0 boto boto3 botocore
+pip install ansible==2.8.0 boto boto3 botocore pyOpenSSL
 git clone https://github.com/CBIIT/icdc-devops
 cd icdc-devops/ansible && git checkout master
 mkdir -p /var/lib/neo4j/conf

91c7166fe8b27662954d3c484069f83f6ed26cb7
Michael Fleming <michael.fleming@nih.gov>
2021-08-19 11:51:32 -0400

    updated release tagging scripts to add timestamp

--- monitoring/releases/set_release.py
+++ monitoring/releases/set_release.py
@@ -26,7 +26,6 @@ def setapmrelease(project, tier, version, key):
    revision_name = '{}-{}-{}'.format(project, tier, version)
    revision_description = '{} {} updated to v{}'.format(project, tier, version)
    revision_time = datetime.utcnow()
-   print(revision_time.isoformat())
    headers = {
        "Api-Key": key,
        "Content-Type": "application/json"
@@ -36,11 +35,12 @@ def setapmrelease(project, tier, version, key):
      "deployment": {
        "revision": revision_name,
        "description": revision_description,
+       "timestamp": revision_time.isoformat(),
      }
    }
 
-#   try:
-#     response = requests.post(API_ENDPOINT, headers=headers, data=json.dumps(data), allow_redirects=False)
-#   except requests.exceptions.RequestException as e:
-#     raise SystemExit(e)
+   try:
+     response = requests.post(API_ENDPOINT, headers=headers, data=json.dumps(data), allow_redirects=False)
+   except requests.exceptions.RequestException as e:
+     raise SystemExit(e)
    print('{} Created'.format(revision_name))

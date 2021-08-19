231f4ced3ea4bb6807ab662023ef6e06f05c71d3
Michael Fleming <michael.fleming@nih.gov>
2021-08-19 11:28:03 -0400

    added release scripts for newrelic

---
+++ monitoring/releases/add_apm_release.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import sys, getopt
+from tags import set_release
+
+def main(argv):
+
+   global project
+   project = ''
+   global tier
+   tier = ''
+   global version
+   version = ''
+   global key
+   key = ''
+
+   try:
+      opts, args = getopt.getopt(argv,"hp:t:v:k:",["project=","tier=","version=","key="])
+   except getopt.GetoptError:
+      print('monitor_query.py -p <project> -t <tier> -v <version> -k <newrelic api key>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('monitor_query.py -p <project> -t <tier> -v <version> -k <api key>')
+         sys.exit()
+      elif opt in ("-p", "--project"):
+         project = arg
+      elif opt in ("-t", "--tier"):
+         tier = arg
+      elif opt in ("-v", "--version"):
+         version = arg
+      elif opt in ("-k", "--key"):
+         key = arg
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+   
+   print()
+   print('Adding APM Release For: {} {} v{}'.format(project, tier, version))
+   print()
+
+   set_release.setapmrelease(project, tier, version, key)
---
+++ monitoring/releases/set_release.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import json
+import requests
+from datetime import datetime
+
+def setapmrelease(project, tier, version, key):
+
+   # get apm app ids
+   API_ENDPOINT = 'https://api.newrelic.com/v2/applications.json'
+
+   headers = {'Api-Key': key}
+
+   try:
+     response = requests.get('{}'.format(API_ENDPOINT), headers=headers)
+   except requests.exceptions.RequestException as e:
+     raise SystemExit(e)
+   
+   for x in response.json()['applications']:
+     if project.lower() in x.get("name", "none").lower() and tier.lower() in x.get("name", "none").lower():
+       apm_id = x.get("id", "none")
+
+   #set release for apm apps
+   API_ENDPOINT = 'https://api.newrelic.com/v2/applications/{}/deployments.json'.format(apm_id)
+
+   revision_name = '{}-{}-{}'.format(project, tier, version)
+   revision_description = '{} {} updated to v{}'.format(project, tier, version)
+   revision_time = datetime.utcnow()
+   print(revision_time.isoformat())
+   headers = {
+       "Api-Key": key,
+       "Content-Type": "application/json"
+   }
+   
+   data = {
+     "deployment": {
+       "revision": revision_name,
+       "description": revision_description,
+     }
+   }
+
+#   try:
+#     response = requests.post(API_ENDPOINT, headers=headers, data=json.dumps(data), allow_redirects=False)
+#   except requests.exceptions.RequestException as e:
+#     raise SystemExit(e)
+   print('{} Created'.format(revision_name))

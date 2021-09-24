f563227a89f72e5f0356454d865cbd788526df09
Austin Mueller <austinsmueller@gmail.com>
2021-09-24 11:57:30 -0400

    Added unit tests
    -Removed unused imports from GraphQLController.java
    -Created GraphQLControllerTest.java
    -Created IndexControllerTest.java

---
+++ src/test/java/gov/nih/nci/bento/IndexControllerTest.java
@@ -0,0 +1,48 @@
+package gov.nih.nci.bento;
+
+import gov.nih.nci.bento.controller.IndexController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(IndexController.class)
+public class IndexControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * Confirm that the "/ping" endpoint accept GET requests and verify the following within the response:
+     *     Http Status Code is 200 (OK)
+     *     Content Type is "text/plain;charset=ISO-8859-1"
+     *     Content matches the String "pong"
+     *
+     * @throws Exception
+     */
+    @Test
+    public void pingEndpointTestGET() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/ping"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
+                .andExpect(MockMvcResultMatchers.content().string("pong"));
+    }
+
+    /**
+     * Confirm that the "/ping" endpoint does NOT accept POST requests and verify the following within the response:
+     *     Http Status Code is 405 (METHOD NOT ALLOWED)
+     *
+     * @throws Exception
+     */
+    @Test
+    public void pingEndpointTestPOST() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/ping"))
+                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+    }
+
+}

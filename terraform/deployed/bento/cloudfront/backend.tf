b2ed56bed34812d53f5457ee1aa71a0db9db0d6a
cloudprofessionals <mycloudprofessionals@gmail.com>
2021-12-01 18:45:24 -0500

    fixed backend pcdc

--- terraform/deployed/bento/cloudfront/backend.tf
+++ terraform/deployed/bento/cloudfront/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket = "bento-terraform-remote-state"
-    key = "bento/ppdc/terraform.tfstate"
+    key = "bento/cloudfront/terraform.tfstate"
     workspace_key_prefix = "env"
     region = "us-east-1"
     encrypt = true

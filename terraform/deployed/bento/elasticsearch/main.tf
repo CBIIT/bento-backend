823e91e37299190f3a0fc2f5f2bbee1d7fe44d60
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-01-25 10:15:00 -0500

    updated elasticsearch count

--- terraform/deployed/bento/elasticsearch/main.tf
+++ terraform/deployed/bento/elasticsearch/main.tf
@@ -50,12 +50,12 @@ resource "aws_elasticsearch_domain" "es" {
   elasticsearch_version = var.elasticsearch_version
 
   cluster_config {
-    instance_count = 2
+    instance_count = 1
     instance_type = var.elasticsearch_instance_type
     zone_awareness_enabled = true
 
     zone_awareness_config {
-      availability_zone_count = 2
+      availability_zone_count = 1
     }
   }
 

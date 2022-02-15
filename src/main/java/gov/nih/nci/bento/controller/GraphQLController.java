571763ef7bf7580467874490ee34d657a553b1ae
knockknockyoo <iheaven0129@gmail.com>
2022-02-15 17:00:58 -0500

    Refactored GraphQLController

--- src/main/java/gov/nih/nci/bento/controller/GraphQLController.java
+++ src/main/java/gov/nih/nci/bento/controller/GraphQLController.java
@@ -1,7 +1,6 @@
 package gov.nih.nci.bento.controller;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import gov.nih.nci.bento.error.ApiError;
 import gov.nih.nci.bento.model.ConfigurationDAO;
@@ -20,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @RestController
@@ -30,6 +30,7 @@ public class GraphQLController {
 	private static final Logger logger = LogManager.getLogger(GraphQLController.class);
 	private final ConfigurationDAO config;
 	private final GraphQLBuilder graphQL;
+	private static final Gson gson = new Gson();
 
 	@CrossOrigin
 	@RequestMapping(value = "/version", method = {RequestMethod.GET}, produces = MediaType.APPLICATION_JSON_VALUE + "; charset=utf-8")
@@ -58,41 +59,43 @@ public class GraphQLController {
 	public ResponseEntity<String> getGraphQLResponse(HttpEntity<String> httpEntity, HttpServletResponse response){
 
 		logger.info("hit end point:/v1/graphql/");
-
 		// Get graphql query from request
-		String reqBody = httpEntity.getBody().toString();
-		Gson gson = new Gson();
-		JsonObject jsonObject = gson.fromJson(reqBody, JsonObject.class);
-		String query;
-		Map<String, Object> variables;
-		String operation;
-		try{
-			query = new String(jsonObject.get("query").getAsString().getBytes(), "UTF-8");
-			JsonElement rawVar = jsonObject.get("variables");
-			variables = gson.fromJson(rawVar, Map.class);
-			Parser parser = new Parser();
-			Document document = parser.parseDocument(query);
-			OperationDefinition def = (OperationDefinition) document.getDefinitions().get(0);
-			operation = def.getOperation().toString().toLowerCase();
-		}
-		catch(Exception e){
-			HttpStatus status = HttpStatus.BAD_REQUEST;
-			String error = ApiError.jsonApiError(status, "Invalid query in request", e.getMessage());
-			return logAndReturnError(status, error);
+		String operation = new String();
+		try {
+			String body = httpEntity.getBody().toString();
+			JsonObject jsonObject = gson.fromJson(body, JsonObject.class);
+			operation = getOperation(jsonObject);
+
+			Map<String, Object> variables = gson.fromJson(jsonObject.get("variables"), Map.class);
+			if (isValid(operation)) return ResponseEntity.ok(graphQL.query(getQuery(jsonObject), variables));
+
+		} catch(Exception e) {
+			String error = ApiError.jsonApiError(HttpStatus.BAD_REQUEST, "Invalid query in request", e.getMessage());
+			logger.error(error);
 		}
 
-		if ((operation.equals("query") && config.isAllowGraphQLQuery())
-				|| (operation.equals("mutation") && config.isAllowGraphQLMutation())) {
-			return ResponseEntity.ok(graphQL.query(query, variables));
+		if (operation.equals("query") || operation.equals("mutation")) {
+			String error = ApiError.jsonApiError(HttpStatus.FORBIDDEN, "Request type has been disabled", operation+" operations have been disabled in the application configuration.");
+			return logAndReturnError(HttpStatus.FORBIDDEN, error);
 		}
-		else if(operation.equals("query") || operation.equals("mutation")){
-			HttpStatus status = HttpStatus.FORBIDDEN;
-			String error = ApiError.jsonApiError(status, "Request type has been disabled", operation+" operations have been disabled in the application configuration.");
-			return logAndReturnError(status, error);
-		}
-		HttpStatus status = HttpStatus.BAD_REQUEST;
-		String error = ApiError.jsonApiError(status, "Unknown operation in request", operation+" operation is not recognized.");
-		return logAndReturnError(status, error);
+		String error = ApiError.jsonApiError(HttpStatus.BAD_REQUEST, "Unknown operation in request", operation+" operation is not recognized.");
+		return logAndReturnError(HttpStatus.BAD_REQUEST, error);
+	}
+
+	private boolean isValid(String operation) {
+		return (operation.equals("query") && config.isAllowGraphQLQuery())
+				|| (operation.equals("mutation") && config.isAllowGraphQLMutation());
+	}
+
+	private String getOperation(JsonObject jsonObject) {
+		Parser parser = new Parser();
+		Document document = parser.parseDocument(getQuery(jsonObject));
+		OperationDefinition def = (OperationDefinition) document.getDefinitions().get(0);
+		return def.getOperation().toString().toLowerCase();
+	}
+
+	private String getQuery(JsonObject jsonObject) {
+		return new String(jsonObject.get("query").getAsString().getBytes(), StandardCharsets.UTF_8);
 	}
 
 	private ResponseEntity logAndReturnError(HttpStatus status, String error){

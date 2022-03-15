b926824ef33d23107e5957f2c41d5819bbd6244a
knockknockyoo <iheaven0129@gmail.com>
2022-03-15 16:02:17 -0400

    Updated ConfigDao Test

--- src/test/java/gov/nih/nci/bento/model/ConfigurationDAOTest.java
+++ src/test/java/gov/nih/nci/bento/model/ConfigurationDAOTest.java
@@ -1,7 +1,8 @@
-package model;
+package gov.nih.nci.bento.model;
 
 import gov.nih.nci.bento.config.ConfigurationDAO;
 import gov.nih.nci.bento.search.datafetcher.DataFetcher;
+import lombok.RequiredArgsConstructor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,20 +10,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith( SpringRunner.class )
 @SpringBootTest
 public class ConfigurationDAOTest {
 
-    // TODO No Bean
     @Autowired
-    public ConfigurationDAO dao;
+    ConfigurationDAO config;
 
     /**
      * Confirm that the data fetcher at least return one configuration file:
      */
     @Test
     public void dataFetcher_Test() {
-        DataFetcher fetcher = dao.dataFetcher();
+        DataFetcher fetcher = config.dataFetcher();
         assertThat(fetcher).isNotNull();
     }
 

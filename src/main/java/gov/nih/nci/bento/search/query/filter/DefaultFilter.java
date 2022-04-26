e2a731a20269fde6aa1c40fe006952306fc848cc
knockknockyoo <iheaven0129@gmail.com>
2022-04-26 16:08:38 -0400

    Bug-Fix - Unable TO Sort When Loading All Data

--- src/main/java/gov/nih/nci/bento/search/query/filter/DefaultFilter.java
+++ src/main/java/gov/nih/nci/bento/search/query/filter/DefaultFilter.java
@@ -1,7 +1,9 @@
 package gov.nih.nci.bento.search.query.filter;
 
 import gov.nih.nci.bento.classes.FilterParam;
+import gov.nih.nci.bento.constants.Const;
 import gov.nih.nci.bento.search.query.QueryFactory;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 public class DefaultFilter extends AbstractFilter {
@@ -13,9 +15,9 @@ public class DefaultFilter extends AbstractFilter {
     @Override
     SearchSourceBuilder getFilter(FilterParam param, QueryFactory bentoParam, boolean loadAllData) {
         SearchSourceBuilder builder = new SearchSourceBuilder()
-                .query(bentoParam.getQuery());
+                .query(loadAllData ? QueryBuilders.matchAllQuery() : bentoParam.getQuery());
         if (param.getSize()!=0) builder.size(param.getSize());
-
+        if (loadAllData) builder.size(Const.ES_UNITS.MAX_SIZE);
         return builder;
     }
 }

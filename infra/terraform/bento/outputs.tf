a844bcd6dbc2ab3e0e2c32b5136848f919742530
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-06-12 22:41:28 -0400

    added fix for dns

--- infra/terraform/bento/outputs.tf
+++ infra/terraform/bento/outputs.tf
@@ -1,6 +1,6 @@
-output "db_private_ip" {
-  value = module.neo4j.private_ip
-}
-output "opensearch_endpoint" {
-  value = module.opensearch.opensearch_endpoint
-}
+//output "db_private_ip" {
+//  value = module.neo4j.private_ip
+//}
+//output "opensearch_endpoint" {
+//  value = module.opensearch.opensearch_endpoint
+//}

57b5f0d2eb447d42b9d45e06da2840e1d9036d26
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-06-30 15:32:51 -0400

    added users ms

--- terraform/deployed/bento/app/ecr.tf
+++ terraform/deployed/bento/app/ecr.tf
@@ -1,38 +1,38 @@
 
-resource "aws_ecr_repository" "ecr" {
-  name = "${lower(var.stack_name)}-${terraform.workspace}"
-  tags = merge(
-  {
-    "Name" = format("%s-%s-%s",var.stack_name,terraform.workspace,"ecr-registry")
-  },
-  var.tags,
-  )
-}
-
-resource "aws_ecr_repository_policy" "ecr_policy" {
-  repository = aws_ecr_repository.ecr.name
-  policy     = data.aws_iam_policy_document.ecr_policy_doc.json
-}
-
-data "aws_iam_policy_document" "ecr_policy_doc" {
-
-  statement {
-    sid    = "ElasticContainerRegistryPushAndPull"
-    effect = "Allow"
-
-    principals {
-      identifiers = [local.my_account]
-      type        = "AWS"
-    }
-    actions = [
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:BatchGetImage",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:PutImage",
-      "ecr:InitiateLayerUpload",
-      "ecr:UploadLayerPart",
-      "ecr:CompleteLayerUpload",
-    ]
-  }
-}
+#resource "aws_ecr_repository" "ecr" {
+#  name = "${lower(var.stack_name)}-${terraform.workspace}"
+#  tags = merge(
+#  {
+#    "Name" = format("%s-%s-%s",var.stack_name,terraform.workspace,"ecr-registry")
+#  },
+#  var.tags,
+#  )
+#}
+#
+#resource "aws_ecr_repository_policy" "ecr_policy" {
+#  repository = aws_ecr_repository.ecr.name
+#  policy     = data.aws_iam_policy_document.ecr_policy_doc.json
+#}
+#
+#data "aws_iam_policy_document" "ecr_policy_doc" {
+#
+#  statement {
+#    sid    = "ElasticContainerRegistryPushAndPull"
+#    effect = "Allow"
+#
+#    principals {
+#      identifiers = [local.my_account]
+#      type        = "AWS"
+#    }
+#    actions = [
+#      "ecr:GetDownloadUrlForLayer",
+#      "ecr:BatchGetImage",
+#      "ecr:BatchCheckLayerAvailability",
+#      "ecr:PutImage",
+#      "ecr:InitiateLayerUpload",
+#      "ecr:UploadLayerPart",
+#      "ecr:CompleteLayerUpload",
+#    ]
+#  }
+#}
 

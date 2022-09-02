fde1dd1799ede7df6e05f5be6586a617b7855b0b
Austin Mueller <64418315+AustinSMueller@users.noreply.github.com>
2022-09-02 15:59:14 -0400

    Migrated simple count queries to elasticsearch
    - Added lab procedures elasticsearch index
    - Added simple count queries to elasricsearch public API

--- src/main/java/gov/nih/nci/bento/model/PublicESDataFetcher.java
+++ src/main/java/gov/nih/nci/bento/model/PublicESDataFetcher.java
@@ -1,9 +1,11 @@
 package gov.nih.nci.bento.model;
 
+import com.google.gson.JsonObject;
 import gov.nih.nci.bento.service.ESService;
 import graphql.schema.idl.RuntimeWiring;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.opensearch.client.Request;
 
 import java.io.IOException;
 import java.util.Map;
@@ -25,7 +27,19 @@ public class PublicESDataFetcher extends AbstractESDataFetcher {
                             Map<String, Object> args = env.getArguments();
                             return globalSearch(args);
                         })
+                        .dataFetcher("numberOfPrograms", env -> getNodeCount(PROGRAMS_COUNT_END_POINT))
+                        .dataFetcher("numberOfStudies", env -> getNodeCount(STUDIES_COUNT_END_POINT))
+                        .dataFetcher("numberOfSubjects", env -> getNodeCount(SUBJECTS_COUNT_END_POINT))
+                        .dataFetcher("numberOfSamples", env -> getNodeCount(SAMPLES_COUNT_END_POINT))
+                        .dataFetcher("numberOfLabProcedures", env -> getNodeCount(LAB_PROCEDURE_COUNT_END_POINT))
+                        .dataFetcher("numberOfFiles", env -> getNodeCount(FILES_COUNT_END_POINT))
                 )
                 .build();
+    }
+
+    private int getNodeCount(String countEndpoint) throws IOException {
+        Request countRequest = new Request("GET", countEndpoint);
+        JsonObject countResult = esService.send(countRequest);
+        return countResult.get("count").getAsInt();
     }
 }

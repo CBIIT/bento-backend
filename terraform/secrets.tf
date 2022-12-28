1d42517828b9d7a8b47bdb2f5b8415e7a6d1ae90
Michael Fleming <michael.fleming@nih.gov>
2022-12-28 15:10:07 -0500

    updated secrets file

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,39 +1,39 @@
+locals {
+
+  dynamic_secrets = {
+    app = {
+      secretKey = ""
+      description = ""
+      secretValue = {
+        es_host = var.create_opensearch_cluster ? module.opensearch[0].opensearch_endpoint : ""
+        sumo_collector_token_frontend = module.monitoring.sumo_source_urls.frontend[0]
+        sumo_collector_token_backend  = module.monitoring.sumo_source_urls.backend[0]
+        sumo_collector_token_files    = module.monitoring.sumo_source_urls.files[0]
+      }
+    }
+  }
+
+}
+
 #secrets
-variable "create_shared_secrets" {
-  type        = bool
-  default     = false
-  description = "shared secrets are created in this tier"
+variable "secret_values" {
+  type = map(object({
+    secretKey = string
+    secretValue = map(string)
+    description = string
+  }))
 }
 
-variable "neo4j_password" {
-  type        = string
-  description = "neo4j password"
-}
-variable "neo4j_ip" {
-  type        = string
-  description = "name of the db instance for this tier"
-}
-variable "indexd_url" {
-  type        = string
-  description = "indexd url"
-}
-
-variable "github_token" {
-  type        = string
-  description = "github token"
-  sensitive   = true
+module "deepmerge" {
+  source  = "Invicton-Labs/deepmerge/null"
+  maps = [
+    local.dynamic_secrets,
+    var.secret_values
+  ]
 }
 
 module "secrets" {
   source                        = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/secrets?ref=terraform_modules"
-  create_shared_secrets         = var.create_shared_secrets
   app                           = var.stack_name
-  es_host                       = var.create_opensearch_cluster ? module.opensearch[0].opensearch_endpoint : ""
-  neo4j_password                = var.neo4j_password
-  neo4j_ip                      = var.neo4j_ip
-  indexd_url                    = var.indexd_url
-  github_token                  = var.github_token
-  sumo_collector_token_frontend = module.monitoring.sumo_source_urls.frontend[0]
-  sumo_collector_token_backend  = module.monitoring.sumo_source_urls.backend[0]
-  sumo_collector_token_files    = module.monitoring.sumo_source_urls.files[0]
+  secret_values                 = module.deepmerge.merged
 }
